16dbefb0ca09f8b931880eb899a90ad5e06fd00b
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2022-11-03 00:51:02 +0200

    error return _cap

--- contracts/SilvaToken.sol
+++ contracts/SilvaToken.sol
@@ -1,41 +1,47 @@
-// contracts/GLDToken.sol
+// contracts/OceanToken.sol
 // SPDX-License-Identifier: MIT
-// WHAT VERSION SOLIDITY TO USE 
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-
-contract OceanToken is ERC20Capped, ERC20Burnable {
-
+contract SilvaToken is ERC20Capped, ERC20Burnable {
     address payable public owner;
     uint256 public blockReward;
 
     constructor(uint256 cap, uint256 reward) ERC20("SilvaToken", "SLV") ERC20Capped(cap * (10 ** decimals())) {
-         owner = payable(msg.sender);
+        owner = payable(msg.sender);
         _mint(owner, 70000000 * (10 ** decimals()));
         blockReward = reward * (10 ** decimals());
+    }
 
+    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
     }
-        //mint miner reward
-          function _mintMinerReward() internal {
+
+    function _mintMinerReward() internal {
         _mint(block.coinbase, blockReward);
+        
     }
-     function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
+   //  function cap() public view virtual returns (uint256 , uint256 reward) {
+       // return _cap;
+   // }
+
+    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
         if(from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
             _mintMinerReward();
         }
         super._beforeTokenTransfer(from, to, value);
     }
 
-    //create function to set the block reward if e decide we want to change it later 
-          function setBlockReward(uint256 reward) public onlyOwner {
+    function setBlockReward(uint256 reward) public onlyOwner {
         blockReward = reward * (10 ** decimals());
     }
-       //destroy contract
-      function destroy() public onlyOwner {
+
+    function destroy() public onlyOwner {
         selfdestruct(owner);
     }
 
@@ -43,7 +49,4 @@ contract OceanToken is ERC20Capped, ERC20Burnable {
         require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
-
-
 }
- 
