6019deb13261b1f802d1c2248f470458e05033ff
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2022-11-03 13:14:00 +0200

    deploye smart contract

--- scripts/deploy_mytoken.js
+++ scripts/deploy_mytoken.js
@@ -4,7 +4,7 @@ const hre = require("hardhat");
 
 async function main() {
   const SilvaToken = await hre.ethers.getContractFactory("SilvaToken");
-  const sl = await SilvaToken.deploy(100000000);
+  const sl = await SilvaToken.deploy(100000000 , 50);
   
   await sl.deployed();
 
